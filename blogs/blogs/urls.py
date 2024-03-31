17f329fb15f3dcf69add1a70bf2a15e1592c9e35
99mandip <karkimandip826@gmail.com>
2024-03-31 20:46:00 +0545

    django second day

--- blogs/blogs/urls.py
+++ blogs/blogs/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.urls import include, re_path 
+# from django.conf.urls import url
+from boards import views 
 urlpatterns = [
+    path('', views.home, name='home'),
+    # re_path(r'^$', views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
