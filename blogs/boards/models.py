da7745e12da69464ec397aa38498f4062a3eadbd
99mandip <karkimandip826@gmail.com>
2024-04-01 11:01:49 +0545

    Added the templates folder and user db shell

--- blogs/boards/models.py
+++ blogs/boards/models.py
@@ -10,6 +10,9 @@ class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name 
+
 
 
 class Topic(models.Model):
--- blogs/boards/views.py
+++ blogs/boards/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse 
+from .models import Board 
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    boards = Board.objects.all()
+    # boards_names = []
+
+    # for board in boards:
+    #     boards_names.append(board.name)
+
+    # response_html = '<br>'.join(boards_names)
+
+    # return HttpResponse(response_html)
+    return render(request, 'home.html', {'boards': boards})
