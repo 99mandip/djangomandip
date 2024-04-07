0781824b2565742242cdb65d507c25d497cf58d4
99mandip <karkimandip826@gmail.com>
2024-04-07 13:17:01 +0545

    created new topics and added font

--- blogs/blogs/urls.py
+++ blogs/blogs/urls.py
@@ -30,5 +30,6 @@ urlpatterns = [
     
     re_path(r'^about/$', views.about, name='about'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
 ]
--- blogs/boards/views.py
+++ blogs/boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse, Http404
 from .models import Board 
@@ -40,3 +40,7 @@ def user_profile(request, username):
 
 def year_archive(request, year):
     return HttpResponse(f"Year: {year}") 
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'new_topic.html', {'board': board})
